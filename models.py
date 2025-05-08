3bec5cc075b38d347383dd66a7a4fe9a702cffed
Ianndav <davide.iann.di@gmail.com>
2025-05-08 21:12:08 +0200

    Update models.py

--- models.py
+++ models.py
@@ -6,4 +6,3 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
-
