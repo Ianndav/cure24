c948b48ff3e772065e7c3df0a3e370e11a21ff54
Ianndav <davide.iann.di@gmail.com>
2025-05-08 20:55:34 +0200

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,14 +1,23 @@
-from flask import Blueprint, render_template, redirect, url_for, flash, request
-from flask_login import login_user, logout_user, current_user
-from models import User
+from flask import Blueprint, render_template, redirect, url_for, flash
+from flask_login import current_user
+from models import db, User
+from forms import RegistrationForm
 
 routes_bp = Blueprint('routes', __name__)
 
 @routes_bp.route('/')
 def home():
-    return render_template('home.html', title='Welcome to Cure24')
+    return render_template('home.html', title='Benvenuto su Cure24!')
 
-@routes_bp.route('/login', methods=['GET', 'POST'])
-def login():
-    # Esempio semplice. Dovresti avere una LoginForm qui.
-    return "Pagina di login (da implementare)"
+@routes_bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('routes.home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Account creato con successo! Puoi ora effettuare il login.', 'success')
+        return redirect(url_for('routes.home'))
+    return render_template('register.html', form=form)
